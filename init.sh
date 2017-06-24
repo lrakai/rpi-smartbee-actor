de4b4a49b00d8834cc3b57ed3d933ac591ec9e5b
Logan Rakai <lmrakai@gmail.com>
2017-06-25 01:59:30 +0800

    init and run scripts

---
+++ init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Upgrade and install firefox and pip
+sudo apt-get update
+sudo apt-get -y dist-upgrade
+sudo apt-get install -y firefox python-pip
+sudo apt-get clean
+curl -LO https://github.com/mozilla/geckodriver/releases/download/v0.17.0/geckodriver-v0.17.0-linux64.tar.gz
+sudo tar -zxf geckodriver-v0.17.0-linux64.tar.gz -C /usr/local/bin
+rm geckodriver-v0.17.0-linux64.tar.gz
+
+pip install virtualenv
+
+# Create a virtual environment for the script
+mkdir browser-sign-in
+virtualenv browser-sign-in
+source browser-sign-in/bin/activate
+pip install -U selenium configparser
+deactivate
+
+# Ensure permission to execute the run script
+chmod u+x run.sh
---
+++ run.sh
@@ -0,0 +1,4 @@
+source browser-sign-in/bin/activate
+python browser_start.py
+python browser_sign_in.py
+deactivate
