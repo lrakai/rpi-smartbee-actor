846ff83fc6525caf96cf59957457b0a4e721ab85
Logan Rakai <lmrakai@gmail.com>
2017-06-25 20:44:07 +0800

    kill detached processes

--- run.sh
+++ run.sh
@@ -6,6 +6,8 @@ if [[ $# -eq 1 ]]; then
 fi
 
 rm -f geckodriver.log
+ps | grep geckodriver | awk '{print $1}' | xargs kill &> /dev/null
+ps | grep firefox | awk '{print $1}' | xargs kill &> /dev/null
 source smartbee-actor/bin/activate
 python $script
 deactivate
