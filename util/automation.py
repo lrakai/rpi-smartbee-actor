8709fe6351ef4d72835f4e24054f8fdafb9809fb
Logan Rakai <lmrakai@gmail.com>
2017-06-25 14:57:02 +0800

    actor logging

--- util/automation.py
+++ util/automation.py
@@ -1,6 +1,8 @@
 ''' Class to act on the webdriver with pre-baked actions '''
 
 from selenium.webdriver.common.keys import Keys
+from datetime import datetime
+import inspect
 
 
 class Actor:
@@ -10,8 +12,13 @@ class Actor:
         self.driver = driver
         self.config = config
 
+    def _log(self, message):
+        ''' log message to output '''
+        print(datetime.isoformat() + "\tActor\t" + message)
+
     def write_session(self):
         ''' write driver session details to .session file '''
+        self._log(inspect.stack()[0][3])
         session_file = open(".session", mode="w")
         session_file.writelines(
             "\n".join([self.driver.command_executor._url,
@@ -20,6 +27,7 @@ class Actor:
 
     def initialize(self):
         ''' initialize the browser for acting upon '''
+        self._log(inspect.stack()[0][3])
         self.driver.maximize_window()
         self.driver.get_base_url()
         self.driver.find_element_by_class_name(
@@ -27,21 +35,25 @@ class Actor:
 
     def login(self):
         ''' login when from the login page '''
+        self._log(inspect.stack()[0][3])
         self.driver.find_element_by_id(
             "password").send_keys(self.config.get_password())
         self.driver.find_element_by_name("Submit").click()
 
     def go_to_base_url(self):
         ''' go to landing page of the configured site '''
+        self._log(inspect.stack()[0][3])
         self.driver.get(self.config.get_base_url())
 
     def go_to_room(self):
         ''' go to configured room '''
+        self._log(inspect.stack()[0][3])
         self.driver.get(self.config.get_room_url())
         self.driver.find_element_by_id("chartingGroup")
 
     def is_logged_out(self):
         ''' Test if the user has been logged out '''
+        self._log(inspect.stack()[0][3])
         error_container = self.driver.find_element_by_class_name(
             "sb-login-page-content-box-error-container-content")
         if error_container is not None:
