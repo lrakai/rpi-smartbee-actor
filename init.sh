d5027c95a7f87eb541d3658a657ef9177e1d5d73
Logan Rakai <lrakai@users.noreply.github.com>
2017-06-27 18:54:06 +0800

    Support arm architectures

--- init.sh
+++ init.sh
@@ -5,9 +5,11 @@ sudo apt-get update
 sudo apt-get -y dist-upgrade
 sudo apt-get install -y firefox python-pip
 sudo apt-get clean
-curl -LO https://github.com/mozilla/geckodriver/releases/download/v0.17.0/geckodriver-v0.17.0-linux64.tar.gz
-sudo tar -zxf geckodriver-v0.17.0-linux64.tar.gz -C /usr/local/bin
-rm geckodriver-v0.17.0-linux64.tar.gz
+# geckodriver=geckodriver-v0.17.0-linux64 # For x86_64 architectures
+geckodriver=geckodriver-v0.17.0-arm7hf # For ARM architectures
+curl -LO https://github.com/mozilla/geckodriver/releases/download/v0.17.0/${geckodriver}.tar.gz
+sudo tar -zxf ${geckodriver}.tar.gz -C /usr/local/bin
+rm ${geckodriver}.tar.gz
 
 pip install virtualenv
 
