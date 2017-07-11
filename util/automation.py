f8e000561c18eec75cb505c59336cd221ef8841f
Logan Rakai <lmrakai@gmail.com>
2017-07-10 23:39:00 -0600

    Return to white default background

--- util/automation.py
+++ util/automation.py
@@ -51,9 +51,9 @@ class Actor:
         self.driver.maximize_window()
         self.go_to_base_url()
         self.driver.execute_script(
-            "document.getElementsByTagName('body')[0].style.background = 'indigo'")
+            "document.getElementsByTagName('body')[0].style.background = 'white'")
         self.driver.execute_script(
-            "document.getElementsByTagName('html')[0].style.background = 'indigo'")
+            "document.getElementsByTagName('html')[0].style.background = 'white'")
         self.full_screen()
 
     def login(self):
